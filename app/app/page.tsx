3146fb2bbc432b9b30619ec683b64d2f1f2b3ff2
lugrizotto <139591963+lugrizotto@users.noreply.github.com>
2025-04-13 17:03:26 -0300

    Delete app/app/page.tsx

--- app/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Bem-vindo à Home Page 🎉</h1>
-      <p>Essa é a rota principal do seu projeto Next.js!</p>
-    </main>
-  )
-}
